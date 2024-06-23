8d9d70d6dbd738572a2b0893b76ea046da8b2bc3
kunalPisolkar24 <kpisolkar24@gmail.com>
2024-06-23 20:49:09 +0530

    added Quick_Sort

---
+++ Quick_Sort/qsort.cpp
@@ -0,0 +1,37 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef vector<long long> vl;
+
+ll partition(vl &arr, ll low, ll high) {
+    ll pivot = arr[high];
+    ll i = low - 1;
+    for(ll j = low; j <= high - 1; j++) {
+        if(arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(vl &arr, ll low, ll high) {
+    if(low < high) {
+        ll pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() { 
+    vl arr = {10, 7, 8, 9, 1, 5};
+
+    quickSort(arr, 0, arr.size() - 1);
+
+    for(ll i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+    return 0;
+}
